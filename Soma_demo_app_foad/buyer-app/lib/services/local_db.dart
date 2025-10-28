6fef0cc98f024aed082e54969a272b8153230980
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-28 21:58:39 +0330

    Update local_db.dart

--- Soma_demo_app_foad/buyer-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/buyer-app/lib/services/local_db.dart
@@ -1,13 +1,14 @@
 class LocalDb {
-  // موجودی‌های اولیه (بدون underscore)
-  static int buyerBalance   = 1000000; // کیف پول/حساب اصلی
-  static int buyerSubsidy   = 500000;  // یارانه
-  static int buyerEmergency = 300000;  // اضطراری
-  static int buyerCBDC      = 200000;  // رمز ارز ملی
+  LocalDb._();
+  static final instance = LocalDb._();
 
-  // Helperهای خیلی ساده برای دمو
-  static void addToWallet(String src, int amount) {
-    switch (src) {
+  int buyerBalance = 1000000;   // موجودی کیف پول
+  int buyerSubsidy = 500000;    // یارانه
+  int buyerEmergency = 300000;  // اضطراری
+  int buyerCBDC = 200000;       // رمز ارز ملی
+
+  void addToWallet(String source, int amount) {
+    switch (source) {
       case 'یارانه':
         buyerSubsidy += amount;
         break;
@@ -19,36 +20,6 @@ class LocalDb {
         break;
       default:
         buyerBalance += amount;
-        break;
     }
-  }
-
-  static bool deductFromWallet(String src, int amount) {
-    switch (src) {
-      case 'یارانه':
-        if (buyerSubsidy >= amount) {
-          buyerSubsidy -= amount;
-          return true;
-        }
-        break;
-      case 'اضطراری':
-        if (buyerEmergency >= amount) {
-          buyerEmergency -= amount;
-          return true;
-        }
-        break;
-      case 'رمز ارز ملی':
-        if (buyerCBDC >= amount) {
-          buyerCBDC -= amount;
-          return true;
-        }
-        break;
-      default:
-        if (buyerBalance >= amount) {
-          buyerBalance -= amount;
-          return true;
-        }
-    }
-    return false;
   }
 }
