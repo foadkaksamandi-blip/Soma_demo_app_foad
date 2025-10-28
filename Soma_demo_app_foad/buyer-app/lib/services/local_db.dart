e6b01f757bd7deaae8ef54ac6d5974acbecccea9
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-28 20:41:13 +0330

    Update local_db.dart

--- Soma_demo_app_foad/buyer-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/buyer-app/lib/services/local_db.dart
@@ -1,22 +1,55 @@
-import 'package:intl/intl.dart';
-
+// buyer-app/lib/services/local_db.dart
+/// Local in-memory DB (demo)
+/// NOTE: این دمو است و پایدار نیست. برای PoC کافی است.
 class LocalDB {
-  static final LocalDB instance = LocalDB._internal();
-  LocalDB._internal();
+  LocalDB._();
+  static final LocalDB instance = LocalDB._();
 
-  int buyerBalance = 500000;
-  int merchantBalance = 0;
+  // موجودی‌های خریدار (چهار کیف مستقل برای دمو)
+  int buyerBalance = 1_000_000;       // نقد/عادی
+  int buyerSubsidy = 500_000;         // یارانه
+  int buyerEmergency = 300_000;       // اضطراری
+  int buyerCBDC = 200_000;            // رمز ارز ملی
 
-  void addBuyerBalance(int amount) {
-    buyerBalance += amount;
+  /// برگرداندن موجودی یک کیف بر اساس منبع انتخابی
+  int getWalletBalance(String source) {
+    switch (source) {
+      case 'یارانه':
+        return buyerSubsidy;
+      case 'اضطراری':
+        return buyerEmergency;
+      case 'رمز ارز ملی':
+        return buyerCBDC;
+      default:
+        return buyerBalance;
+    }
   }
 
-  void addMerchantBalance(int amount) {
-    merchantBalance += amount;
+  /// بروزرسانی موجودی یک کیف (delta می‌تواند منفی باشد)
+  void addToWallet(String source, int delta) {
+    switch (source) {
+      case 'یارانه':
+        buyerSubsidy += delta;
+        if (buyerSubsidy < 0) buyerSubsidy = 0;
+        break;
+      case 'اضطراری':
+        buyerEmergency += delta;
+        if (buyerEmergency < 0) buyerEmergency = 0;
+        break;
+      case 'رمز ارز ملی':
+        buyerCBDC += delta;
+        if (buyerCBDC < 0) buyerCBDC = 0;
+        break;
+      default:
+        buyerBalance += delta;
+        if (buyerBalance < 0) buyerBalance = 0;
+        break;
+    }
   }
 
-  static String formatRials(int value) {
-    final f = NumberFormat.decimalPattern('fa');
-    return f.format(value);
+  /// افزایش تستی موجودی عادی (برای FAB)
+  void addBuyerBalance(int delta) {
+    buyerBalance += delta;
+    if (buyerBalance < 0) buyerBalance = 0;
   }
 }
