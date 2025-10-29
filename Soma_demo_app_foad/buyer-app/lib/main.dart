5536131e16fd15e515fef794f53adf8ad75ab0f8
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-29 18:31:34 +0330

    Update main.dart

--- Soma_demo_app_foad/buyer-app/lib/main.dart
+++ Soma_demo_app_foad/buyer-app/lib/main.dart
@@ -1,43 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart';
-
-import 'screens/scan_qr_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  // فرمت پیش‌فرض به فارسی/انگلیسی
-  Intl.defaultLocale = 'fa';
-  runApp(const BuyerApp());
+  runApp(const SomaBuyerApp());
 }
 
-class BuyerApp extends StatelessWidget {
-  const BuyerApp({super.key});
+class SomaBuyerApp extends StatelessWidget {
+  const SomaBuyerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'اپ آفلاین سوما – خریدار',
+      title: 'SOMA Offline Buyer App',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const BuyerHomeScreen(),
       debugShowCheckedModeBanner: false,
-      locale: const Locale('fa'),
-      supportedLocales: const [
-        Locale('fa'),
-        Locale('en'),
-      ],
-      // ⚠️ این‌ها نباید const باشند
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'Roboto',
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF19B394)),
-      ),
-      initialRoute: '/scan/qr',
-      routes: {
-        '/scan/qr': (_) => const QrPayScreen(), // همون فایل فعلی تو مسیر screens/scan_qr_screen.dart
-      },
     );
   }
 }
