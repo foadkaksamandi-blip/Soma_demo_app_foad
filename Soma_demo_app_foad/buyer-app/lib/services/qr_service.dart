9bb2e40d7300a4583da7a2b6aab10f187aa084cd
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-29 20:14:22 +0330

    Update qr_service.dart

--- Soma_demo_app_foad/buyer-app/lib/services/qr_service.dart
+++ Soma_demo_app_foad/buyer-app/lib/services/qr_service.dart
@@ -1,27 +1,64 @@
 import 'dart:convert';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter/material.dart';
 
-class BuyerQrService {
-  const BuyerQrService();
-
-  String buildPayload({required int amount, required String txId}) {
-    final map = {
-      'type': 'soma_tx',
-      'amount': amount,
-      'tx_id': txId,
-      'ts': DateTime.now().millisecondsSinceEpoch,
-    };
-    return jsonEncode(map);
+class QrService {
+  static String generateTransactionQR(Map<String, dynamic> data) {
+    final encoded = jsonEncode(data);
+    return encoded;
   }
 
-  Map<String, dynamic>? parse(String raw) {
-    try {
-      final obj = jsonDecode(raw);
-      if (obj is Map<String, dynamic> && obj['type'] == 'soma_tx' && obj.containsKey('amount') && obj.containsKey('tx_id')) {
-        return obj;
-      }
-      return null;
-    } catch (_) {
-      return null;
-    }
+  static Future<void> scanQR(
+      BuildContext context, void Function(Map<String, dynamic>) onResult) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => QRViewExample(onResult: onResult),
+      ),
+    );
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  final void Function(Map<String, dynamic>) onResult;
+  const QRViewExample({required this.onResult, super.key});
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    controller?.pauseCamera();
+    controller?.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: (ctrl) {
+          controller = ctrl;
+          controller!.scannedDataStream.listen((scanData) {
+            final decoded = jsonDecode(scanData.code ?? '{}');
+            widget.onResult(decoded);
+            Navigator.pop(context);
+          });
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
