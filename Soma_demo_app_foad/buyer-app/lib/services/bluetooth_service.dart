53db382a37aa616989116054cb2b4253408b0cf7
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-29 20:38:46 +0330

    Update bluetooth_service.dart

--- Soma_demo_app_foad/buyer-app/lib/services/bluetooth_service.dart
+++ Soma_demo_app_foad/buyer-app/lib/services/bluetooth_service.dart
@@ -2,40 +2,40 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
+/// سرویس ساده برای اتصال، ارسال و دریافت پیام (UTF8) از طریق Bluetooth RFCOMM
 class BluetoothService {
-  static final BluetoothService _instance = BluetoothService._internal();
-  factory BluetoothService() => _instance;
-  BluetoothService._internal();
+  static final BluetoothService _i = BluetoothService._();
+  BluetoothService._();
+  factory BluetoothService() => _i;
 
-  BluetoothConnection? _connection;
-  final StreamController<String> _onDataReceived = StreamController.broadcast();
+  BluetoothConnection? _conn;
+  final _stream = StreamController<String>.broadcast();
 
-  Stream<String> get onData => _onDataReceived.stream;
+  Stream<String> get onData => _stream.stream;
+  bool get isConnected => _conn?.isConnected ?? false;
 
   Future<bool> connect(String address) async {
     try {
-      _connection = await BluetoothConnection.toAddress(address);
-      _connection!.input!.listen((data) {
-        final message = utf8.decode(data);
-        _onDataReceived.add(message);
-      });
+      _conn = await BluetoothConnection.toAddress(address);
+      _conn!.input?.listen((data) {
+        final msg = utf8.decode(data);
+        _stream.add(msg);
+      }, onDone: () => disconnect());
       return true;
-    } catch (e) {
+    } catch (_) {
       return false;
     }
   }
 
-  Future<void> sendData(String message) async {
-    if (_connection?.isConnected ?? false) {
-      _connection!.output.add(utf8.encode(message));
-      await _connection!.output.allSent;
-    }
+  Future<void> sendJson(Map<String, dynamic> json) async {
+    if (!isConnected) return;
+    final line = jsonEncode(json) + '\n';
+    _conn!.output.add(utf8.encode(line));
+    await _conn!.output.allSent;
   }
 
   void disconnect() {
-    _connection?.dispose();
-    _connection = null;
+    _conn?.dispose();
+    _conn = null;
   }
-
-  bool get isConnected => _connection?.isConnected ?? false;
 }
