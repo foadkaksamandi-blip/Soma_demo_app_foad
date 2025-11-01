d69889df8cec0564aa8af7bd56b951bfd44a1138
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-01 19:31:51 +0330

    Update local_db.dart

--- Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
@@ -1,40 +1,14 @@
-class LocalDBMerchant {
-  LocalDBMerchant._();
-  static final LocalDBMerchant instance = LocalDBMerchant._();
+import 'package:shared_preferences/shared_preferences.dart';
 
-  int _main = 2500000;
-  int _subsidy = 0;
-  int _emergency = 0;
-  int _crypto = 0;
+class LocalDB {
+  LocalDB._();
+  static final LocalDB instance = LocalDB._();
 
-  int get merchantBalance => _main;
-
-  Map<String, int> get balances => {
-        'main': _main,
-        'subsidy': _subsidy,
-        'emergency': _emergency,
-        'crypto': _crypto,
-      };
-
-  Future<void> addIncome({required String wallet, required int amount}) async {
-    if (amount <= 0) return;
-    switch (wallet) {
-      case 'main':
-        _main += amount;
-        break;
-      case 'subsidy':
-        _subsidy += amount;
-        break;
-      case 'emergency':
-        _emergency += amount;
-        break;
-      case 'crypto':
-        _crypto += amount;
-        break;
-      default:
-        _main += amount;
-        break;
-    }
-    await Future<void>.delayed(const Duration(milliseconds: 120));
+  Future<bool> receiveAmount(int amount, String wallet) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'wallet_$wallet';
+    final current = prefs.getInt(key) ?? 0;
+    await prefs.setInt(key, current + amount);
+    return true;
   }
 }
