1dc909632e329789c44cd62932486e6f44f0ec4f
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-01 17:37:28 +0330

    Update local_db.dart

--- Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
@@ -1,10 +1,40 @@
 class LocalDBMerchant {
   LocalDBMerchant._();
-  static final instance = LocalDBMerchant._();
+  static final LocalDBMerchant instance = LocalDBMerchant._();
 
-  int _merchantBalance = 0;
+  int _main = 2500000;
+  int _subsidy = 0;
+  int _emergency = 0;
+  int _crypto = 0;
 
-  Future<int> get balance async => _merchantBalance;
+  int get merchantBalance => _main;
 
-  Future<void> add(int v) async { _merchantBalance += v; }
+  Map<String, int> get balances => {
+        'main': _main,
+        'subsidy': _subsidy,
+        'emergency': _emergency,
+        'crypto': _crypto,
+      };
+
+  Future<void> addIncome({required String wallet, required int amount}) async {
+    if (amount <= 0) return;
+    switch (wallet) {
+      case 'main':
+        _main += amount;
+        break;
+      case 'subsidy':
+        _subsidy += amount;
+        break;
+      case 'emergency':
+        _emergency += amount;
+        break;
+      case 'crypto':
+        _crypto += amount;
+        break;
+      default:
+        _main += amount;
+        break;
+    }
+    await Future<void>.delayed(const Duration(milliseconds: 120));
+  }
 }
