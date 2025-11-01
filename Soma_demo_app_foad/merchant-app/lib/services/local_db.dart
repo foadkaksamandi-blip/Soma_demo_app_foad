2750a43a653973bf1b809ed291c3e09efec907a2
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-01 21:05:19 +0330

    Update local_db.dart

--- Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
@@ -1,14 +1,40 @@
+import 'dart:math';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalDB {
   LocalDB._();
   static final LocalDB instance = LocalDB._();
 
-  Future<bool> receiveAmount(int amount, String wallet) async {
-    final prefs = await SharedPreferences.getInstance();
-    final key = 'wallet_$wallet';
-    final current = prefs.getInt(key) ?? 0;
-    await prefs.setInt(key, current + amount);
-    return true;
+  static const _kMerchantBalance = 'merchant_balance';
+  static const _kLastTxId = 'merchant_last_tx_id';
+
+  Future<SharedPreferences> get _sp async => SharedPreferences.getInstance();
+
+  Future<void> ensureDefaults() async {
+    final sp = await _sp;
+    if (!sp.containsKey(_kMerchantBalance)) {
+      await sp.setInt(_kMerchantBalance, 0);
+      await sp.setInt(_kLastTxId, 500000 + Random().nextInt(900000));
+    }
+  }
+
+  Future<int> balance() async {
+    final sp = await _sp;
+    return sp.getInt(_kMerchantBalance) ?? 0;
+  }
+
+  Future<bool> receive_amount(int amount) async {
+    await ensureDefaults();
+    if (amount <= 0) return false;
+    final sp = await _sp;
+    final cur = sp.getInt(_kMerchantBalance) ?? 0;
+    return sp.setInt(_kMerchantBalance, cur + amount);
+  }
+
+  Future<String> newTxId() async {
+    final sp = await _sp;
+    final next = (sp.getInt(_kLastTxId) ?? 0) + 1;
+    await sp.setInt(_kLastTxId, next);
+    return next.toString();
   }
 }
