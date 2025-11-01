8cbdda1e79c3e2ba1b6462e72c03b91dbed1141f
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-01 14:48:08 +0330

    Update local_db.dart

--- Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
+++ Soma_demo_app_foad/merchant-app/lib/services/local_db.dart
@@ -1,13 +1,10 @@
-// merchant-app/lib/services/local_db.dart
-/// Local in-memory DB (demo)
 class LocalDBMerchant {
   LocalDBMerchant._();
-  static final LocalDBMerchant instance = LocalDBMerchant._();
+  static final instance = LocalDBMerchant._();
 
-  int merchantBalance = 0;
+  int _merchantBalance = 0;
 
-  void addMerchantBalance(int delta) {
-    merchantBalance += delta;
-    if (merchantBalance < 0) merchantBalance = 0;
-  }
+  Future<int> get balance async => _merchantBalance;
+
+  Future<void> add(int v) async { _merchantBalance += v; }
 }
