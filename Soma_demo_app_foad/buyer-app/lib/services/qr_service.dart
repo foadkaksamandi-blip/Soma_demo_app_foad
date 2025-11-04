3c72b34ec488e97c83937c378cba9dae550e89cd
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-04 19:44:33 +0330

    Update qr_service.dart

--- Soma_demo_app_foad/buyer-app/lib/services/qr_service.dart
+++ Soma_demo_app_foad/buyer-app/lib/services/qr_service.dart
@@ -1,64 +1,23 @@
 import 'dart:convert';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:flutter/material.dart';
+import '../models/tx_log.dart';
 
 class QrService {
-  static String generateTransactionQR(Map<String, dynamic> data) {
-    final encoded = jsonEncode(data);
-    return encoded;
+  static String encode(TxLog tx) {
+    return base64Encode(utf8.encode(jsonEncode(tx.toMap())));
   }
 
-  static Future<void> scanQR(
-      BuildContext context, void Function(Map<String, dynamic>) onResult) async {
-    await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => QRViewExample(onResult: onResult),
-      ),
+  static TxLog decode(String data) {
+    final decoded = utf8.decode(base64Decode(data));
+    return TxLog.fromMap(Map<String, String>.from(jsonDecode(decoded)));
+  }
+
+  static QrImage generateQr(TxLog tx, {double size = 200}) {
+    final data = encode(tx);
+    return QrImage(
+      data: data,
+      version: QrVersions.auto,
+      size: size,
     );
-  }
-}
-
-class QRViewExample extends StatefulWidget {
-  final void Function(Map<String, dynamic>) onResult;
-  const QRViewExample({required this.onResult, super.key});
-
-  @override
-  State<StatefulWidget> createState() => _QRViewExampleState();
-}
-
-class _QRViewExampleState extends State<QRViewExample> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    controller?.pauseCamera();
-    controller?.resumeCamera();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: QRView(
-        key: qrKey,
-        onQRViewCreated: (ctrl) {
-          controller = ctrl;
-          controller!.scannedDataStream.listen((scanData) {
-            final decoded = jsonDecode(scanData.code ?? '{}');
-            widget.onResult(decoded);
-            Navigator.pop(context);
-          });
-        },
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
   }
 }
