67c04b61474439124f50e9b1f44db84e8bc77a67
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-04 19:57:10 +0330

    Update permissions.dart

--- Soma_demo_app_foad/merchant-app/lib/services/permissions.dart
+++ Soma_demo_app_foad/merchant-app/lib/services/permissions.dart
@@ -1,12 +1,22 @@
-// merchant-app/lib/services/permissions.dart
-class AppPermissions {
-  AppPermissions._();
+import 'package:permission_handler/permission_handler.dart';
 
-  static Future<bool> ensureCamera() async {
-    return true;
+class PermissionService {
+  static Future<bool> ensureBluetooth() async {
+    final statuses = await [
+      Permission.bluetooth,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan,
+    ].request();
+    return statuses.values.every((s) => s.isGranted);
   }
 
-  static Future<bool> ensureBluetooth() async {
-    return true;
+  static Future<bool> ensureCamera() async {
+    final status = await Permission.camera.request();
+    return status.isGranted;
+  }
+
+  static Future<bool> ensureLocation() async {
+    final status = await Permission.locationWhenInUse.request();
+    return status.isGranted;
   }
 }
