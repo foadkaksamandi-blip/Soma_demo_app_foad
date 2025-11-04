487abad55924aa3ac7ca7cbe7915a08eb20e0653
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-11-04 19:48:01 +0330

    Update MainActivity.kt

--- Soma_demo_app_foad/merchant-app/android/app/src/main/kotlin/com/soma/foad/merchant/MainActivity.kt
+++ Soma_demo_app_foad/merchant-app/android/app/src/main/kotlin/com/soma/foad/merchant/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.soma.merchant
+package com.soma.foad.merchant
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
